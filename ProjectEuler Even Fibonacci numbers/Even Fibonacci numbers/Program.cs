d3c34dcdc73d6604c06c34f3e09d38733c4c8b72
Luriarix <ionitaandrei_lucian@yahoo.ro>
2020-06-23 00:41:09 +0100

    Fibonacci Even Sum

---
+++ ProjectEuler Even Fibonacci numbers/Even Fibonacci numbers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Even_Fibonacci_numbers
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            FibonacciSequence(10);
+        }
+
+        public static string FibonacciSequence(int num)
+        {
+            //return num.ToString();//initial
+
+            ////step 1
+            //int elementInTheSequence = 1, previousElementInSequence = 1;
+            //while (elementInTheSequence < num)
+            //{
+            //    elementInTheSequence = previousElementInSequence + elementInTheSequence;
+            //    previousElementInSequence = elementInTheSequence - previousElementInSequence;
+            //};
+            //return elementInTheSequence.ToString();
+
+            //step 2
+            int elementInTheSequence = 1, previousElementInSequence = 1, sumOfEvenElements = 0;
+            while (elementInTheSequence <= num)
+            {
+                elementInTheSequence = previousElementInSequence + elementInTheSequence;
+                previousElementInSequence = elementInTheSequence - previousElementInSequence;
+                if (elementInTheSequence % 2 == 0)
+                {
+                    sumOfEvenElements += elementInTheSequence;
+                }
+            };
+            return sumOfEvenElements.ToString();
+        }
+    }
+}
---
+++ ProjectEuler Even Fibonacci numbers/EvenFibonacciNumbersNUnitTest/UnitTest1.cs
@@ -0,0 +1,30 @@
+using NUnit.Framework;
+using Even_Fibonacci_numbers;
+
+namespace EvenFibonacciNumbersNUnitTest
+{
+    public class Tests
+    {
+        //[Test]//initial Given 1 return 1
+        //public void GivenOne_Return_OneString()
+        //{
+        //    Assert.That(Program.FibonacciSequence(1), Is.EqualTo("1"));
+        //}
+
+        //[Test]//test 1 starting the sequence and showing it's last elements, given 2 return 2
+        //public void GivenTwo_Return_LastElement()
+        //{
+        //    Assert.That(Program.FibonacciSequence(2), Is.EqualTo("2"));
+        //}
+
+        [TestCase(10, "10")]//test 2 sum of sequence of even numbers
+        [TestCase(40, "44")]
+        [TestCase(1000, "798")]
+        public void GivenTen_Return_SumOfEvenElements(int num, string expected)
+        {
+            Assert.That(Program.FibonacciSequence(num), Is.EqualTo(expected));
+        }
+
+
+    }
+}
